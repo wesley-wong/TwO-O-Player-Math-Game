ec1aa628f14095b385a7041e2ff4d73db387929a
trrippy <wes.wong@hotmail.com>
2017-02-14 00:26:34 +0000

    :sparkles: Questions now puts question using rand()
    Signed-off-by: trrippy <wes.wong@hotmail.com>

--- questions.rb
+++ questions.rb
@@ -1,7 +1,14 @@
 class Questions
 
   def initialize
+    @num_1 = rand(20)
+    @num_2 = rand(20)
+    @answer = @num_1 + @num_2
+
+    puts "What does #{@num_1} plus #{@num_2} equal?"
 
   end
 
 end
+
+question = Questions.new
